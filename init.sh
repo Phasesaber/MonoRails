4677f7814fde06e9e535442ac2137967d45c537f
Jαdon Fowler <ph4sesaber@gmail.com>
2015-03-18 19:06:02 -0700

    Create init file

---
+++ init.sh
@@ -0,0 +1,11 @@
+git init
+#New git project created
+
+git remote add rails https://github.com/PoweredRails/MonoRails
+#Add the remote to the project
+
+git pull rails master
+#Pull the repo
+
+head -30 README.md
+#Open the README
